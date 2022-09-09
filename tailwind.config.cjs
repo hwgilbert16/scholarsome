e52c75e93c5a2fa23c14a6c1ba975f6f7bd81971
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2022-09-09 11:05:54 -0400

    Remove svelte, moving to angular and nest

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/@brainandbones/skeleton/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-		extend: {}
-	},
-
-	plugins: [require("@brainandbones/skeleton/tailwind.cjs")]
-};
