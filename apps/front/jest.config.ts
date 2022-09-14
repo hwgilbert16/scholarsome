a54334e270dfec001261cbf6ac99ca4a1bcbd87d
hwgilbert16 <hwgilbert16@gmail.com>
2022-09-13 21:02:13 -0400

    Rename front directory

--- apps/front/jest.config.ts
+++ apps/front/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'quizletbutfree',
+  displayName: 'front',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/quizletbutfree',
+  coverageDirectory: '../../coverage/apps/front',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
