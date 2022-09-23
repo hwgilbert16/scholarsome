e8ebb1f4b591f14a701637de3ce45890aacfaca8
hwgilbert16 <hwgilbert16@gmail.com>
2022-09-22 21:27:22 -0400

    Add start of prisma schema

--- apps/front/src/app/shared/modal/modal.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import {Subject} from "rxjs";
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ModalService {
-  registerModalOpen = new Subject()
-}
