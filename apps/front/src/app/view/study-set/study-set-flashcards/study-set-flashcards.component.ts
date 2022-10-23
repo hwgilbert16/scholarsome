2b176654711c29465d441b323b822b901af8d7d2
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2022-10-23 15:06:16 -0400

    Add flashcards page and fix flashcards link

--- apps/front/src/app/view/study-set/study-set-flashcards/study-set-flashcards.component.ts
+++ apps/front/src/app/view/study-set/study-set-flashcards/study-set-flashcards.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { SetsService } from "../../../shared/http/sets.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'scholarsome-study-set-flashcards',
@@ -6,12 +8,30 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./study-set-flashcards.component.scss'],
 })
 export class StudySetFlashcardsComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private sets: SetsService,
+    private router: Router
+  ) {}
 
   @ViewChild('spinner', { static: true }) spinner: ElementRef;
+  @ViewChild('container', { static: true }) container: ElementRef;
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const setId = this.route.snapshot.paramMap.get('setId');
+    if (!setId) {
+      await this.router.navigate(['404']);
+      return;
+    }
+
+    const set = await this.sets.set(setId);
+    if (!set) {
+      await this.router.navigate(['404']);
+      return;
+    }
+
     this.spinner.nativeElement.remove();
+    this.container.nativeElement.removeAttribute('hidden');
   }
 
 }
