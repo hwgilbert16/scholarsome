70230f6065539e9d57f6f132750eb5012ba24094
hwgilbert16 <hwgilbert16@gmail.com>
2022-11-14 20:57:52 -0500

    Add https force in production

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './providers/database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SetsModule } from './sets/sets.module';
 import { MailModule } from './providers/mail/mail.module';
+import { HttpsRedirectMiddleware } from "./providers/https-redirect.middleware";
 
 @Module({
   imports: [
@@ -24,4 +25,12 @@ import { MailModule } from './providers/mail/mail.module';
   controllers: [AuthController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor (private configService: ConfigService) {}
+
+  configure(consumer: MiddlewareConsumer): any {
+    if (this.configService.get<string>('NODE_ENV') === 'production' && this.configService.get<string>('SSL_KEY_PATH') && this.configService.get<string>('SSL_KEY_PATH').length > 0) {
+      consumer.apply(HttpsRedirectMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL });
+    }
+  }
+}
---
+++ apps/api/src/app/providers/https-redirect.middleware.ts
@@ -0,0 +1,14 @@
+import { HttpStatus, Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+@Injectable()
+export class HttpsRedirectMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction): any {
+    if (!req.secure) {
+      const httpsUrl = `https://${req.hostname}${req.originalUrl}`;
+      res.redirect(HttpStatus.PERMANENT_REDIRECT, httpsUrl);
+    } else {
+      next();
+    }
+  }
+}
