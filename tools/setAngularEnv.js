84ca35db8dbe8e4f3e526ef31ba1d396700d7af6
hwgilbert16 <hwgilbert16@gmail.com>
2023-05-07 17:16:17 -0400

    Remove file replacement from project.json

--- tools/setAngularEnv.js
+++ tools/setAngularEnv.js
@@ -1,19 +1,11 @@
 const { writeFile } = require('fs');
 require('dotenv').config();
 
-const prodContent = `
+const fileContent = `
 export const environment = {
-   production: true,
+   production: ${process.env.NODE_ENV === 'production' ? 'true' : 'false'},
    recaptchaSiteKey: '${process.env.RECAPTCHA_SITE}',
 };
 `;
 
-const nonProdContent = `
-export const environment = {
-   production: false,
-   recaptchaSiteKey: '${process.env.RECAPTCHA_SITE}',
-};
-`;
-
-writeFile('./apps/front/src/environments/environment.prod.ts', prodContent, () => {});
-writeFile('./apps/front/src/environments/environment.ts', nonProdContent, () => {});
+writeFile('./apps/front/src/environments/environmet.ts', fileContent, () => {});
