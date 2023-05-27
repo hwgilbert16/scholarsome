d2ce01ff5f5ac9ce0be47c36d2f5935d371863d6
hwgilbert16 <hwgilbert16@gmail.com>
2023-05-26 20:59:42 -0400

    Finish test for AuthController

--- apps/api/src/app/auth/auth.controller.spec.ts
+++ apps/api/src/app/auth/auth.controller.spec.ts
@@ -10,11 +10,13 @@ import { RedisService } from "@liaoliaots/nestjs-redis";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { MailService } from "../providers/mail/mail.service";
 import { Request, Response } from "express";
+import { HttpException } from "@nestjs/common";
 
 describe("AuthController", () => {
   let authController: AuthController;
   let usersService: UsersService;
   let mailService: MailService;
+  let authService: AuthService;
 
   const resetToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNTE2MjM5MDIyLCJyZXNldCI6InRydWUiLCJlbWFpbCI6ImFAYS5jb20ifQ.4xRSOpUWnOUmX24W_cQn3ss4H-qNPB5D84eHLXIg1gQ";
 
@@ -24,21 +26,28 @@ describe("AuthController", () => {
     }
   } as Request;
 
-  const res = {
-    status: jest.fn(),
-    cookie: jest.fn(),
-    redirect: jest.fn()
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } as any as Response;
-
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [
         {
           provide: UsersService,
           useValue: {
-            user: (obj: { email: string }) => {
-              return obj.email === "a";
+            user: (obj: { email?: string, username?: string }) => {
+              switch (obj.email) {
+                case "true":
+                  return true;
+                case "false":
+                  return false;
+              }
+
+              switch (obj.username) {
+                case "true":
+                  return true;
+                case "false":
+                  return false;
+              }
+
+              return {};
             },
             createUser: jest.fn(),
             updateUser: jest.fn().mockResolvedValue({})
@@ -46,7 +55,36 @@ describe("AuthController", () => {
         },
         {
           provide: AuthService,
-          useValue: createMock<AuthService>()
+          useValue: {
+            validateUser: (email: string, password: string) => {
+              switch (password) {
+                case "true":
+                  return true;
+                case "false":
+                  return false;
+              }
+
+              switch (email) {
+                case "true":
+                  return true;
+                case "false":
+                  return false;
+              }
+
+              return {};
+            },
+            validateRecaptcha: (s: string) => {
+              switch (s) {
+                case "true":
+                  return true;
+                case "false":
+                  return false;
+                default:
+                  return {};
+              }
+            },
+            logout: jest.fn()
+          }
         },
         {
           provide: JwtService,
@@ -55,7 +93,17 @@ describe("AuthController", () => {
         {
           provide: MailService,
           useValue: {
-            sendPasswordReset: jest.fn()
+            sendPasswordReset: jest.fn(),
+            sendEmailConfirmation: (s: string) => {
+              switch (s) {
+                case "true":
+                  return true;
+                case "false":
+                  return false;
+                default:
+                  return {};
+              }
+            }
           }
         },
         {
@@ -85,8 +133,12 @@ describe("AuthController", () => {
     authController = module.get(AuthController);
     usersService = module.get(UsersService);
     mailService = module.get(MailService);
+    authService = module.get(AuthService);
 
     jest.spyOn(usersService, "user");
+    jest.spyOn(mailService, "sendEmailConfirmation");
+    jest.spyOn(authService, "validateUser");
+    jest.spyOn(authService, "validateRecaptcha");
   });
 
   it("should be defined", () => {
@@ -97,6 +149,13 @@ describe("AuthController", () => {
     const dto = {
       password: "a"
     };
+
+    const res = {
+      status: jest.fn(),
+      cookie: jest.fn(),
+      redirect: jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any as Response;
 
     it("should throw an error with an invalid resetToken", async () => {
       const req = {
@@ -133,6 +192,13 @@ describe("AuthController", () => {
   });
 
   describe("when the GET /reset/password/setCookie/:token route is called", () => {
+    const res = {
+      status: jest.fn(),
+      cookie: jest.fn(),
+      redirect: jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any as Response;
+
     it("should redirect to /reset", async () => {
       await authController.setResetCookie({ token: "" }, res);
 
@@ -157,19 +223,19 @@ describe("AuthController", () => {
 
   describe("when the GET /reset/password/:email route is called", () => {
     it("should send an email if user is valid", async () => {
-      await authController.sendReset({ email: "a" });
+      await authController.sendReset({ email: "true" });
 
-      expect(mailService.sendPasswordReset).toHaveBeenCalledWith("a");
+      expect(mailService.sendPasswordReset).toHaveBeenCalledWith("true");
     });
 
     it("should not send an email if user is not valid", async () => {
-      await authController.sendReset({ email: "b" });
+      await authController.sendReset({ email: "false" });
 
       expect(mailService.sendPasswordReset).not.toHaveBeenCalled();
     });
 
     it("should return the correct value", async () => {
-      const result = await authController.sendReset({ email: "a" });
+      const result = await authController.sendReset({ email: "true" });
 
       expect(result).toEqual({
         status: "success",
@@ -179,6 +245,13 @@ describe("AuthController", () => {
   });
 
   describe("when the GET /verify/email/:token route is called", () => {
+    const res = {
+      status: jest.fn(),
+      cookie: jest.fn(),
+      redirect: jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any as Response;
+
     it("should update the user if the jwt is verified", async () => {
       await authController.verifyEmail({ token: resetToken }, res);
 
@@ -207,6 +280,179 @@ describe("AuthController", () => {
         status: "fail",
         message: "Invalid token"
       });
+    });
+  });
+
+  describe("when the register route is called", () => {
+    const res = {
+      status: jest.fn(),
+      cookie: jest.fn(),
+      redirect: jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any as Response;
+
+    it("should send HTTP 409 if user already exists", async () => {
+      const dto = {
+        username: "true",
+        email: "true",
+        password: "b",
+        confirmPassword: "b",
+        recaptchaToken: "a"
+      };
+
+      const result = await authController.register(dto, res);
+
+      expect(res.status).toHaveBeenCalledWith(409);
+      expect(result).toEqual({
+        status: "fail",
+        message: "Email already exists"
+      });
+    });
+
+    it("should create user if user does not exist", async () => {
+      const dto = {
+        username: "false",
+        email: "false",
+        password: "b",
+        confirmPassword: "b",
+        recaptchaToken: "a"
+      };
+
+      await authController.register(dto, res);
+
+      expect(usersService.createUser).toHaveBeenCalledWith({
+        username: "false",
+        email: "false",
+        password: expect.any(String),
+        verified: expect.any(Boolean)
+      });
+    });
+
+    it("should not send email if email not enabled", async () => {
+      const dto = {
+        username: "false",
+        email: "false",
+        password: "b",
+        confirmPassword: "b",
+        recaptchaToken: "a"
+      };
+
+      const result = await authController.register(dto, res);
+
+      expect(mailService.sendEmailConfirmation).toHaveBeenCalledWith(dto.email);
+      expect(result).toEqual({
+        status: "success",
+        data: { confirmEmail: false }
+      });
+    });
+  });
+
+  describe("when the login route is called", () => {
+    const res = {
+      status: jest.fn(),
+      cookie: jest.fn(),
+      redirect: jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any as Response;
+
+    it("should send HTTP 401 if incorrect password", async () => {
+      const dto = {
+        email: "false",
+        password: "false",
+        recaptchaToken: "a"
+      };
+
+      const result = await authController.login(dto, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(result).toEqual({
+        status: "fail",
+        message: "Incorrect email or password"
+      });
+    });
+
+    it("should validate recaptcha", async () => {
+      const dto = {
+        email: "true",
+        password: "true",
+        recaptchaToken: "true"
+      };
+
+      await authController.login(dto, res);
+
+      expect(authService.validateRecaptcha).toHaveBeenCalledWith(dto.recaptchaToken);
+    });
+
+    it("should throw an exception if recaptcha is invalid", async () => {
+      const dto = {
+        email: "true",
+        password: "true",
+        recaptchaToken: "false"
+      };
+
+      await expect(async () => {
+        await authController.login(dto, res);
+      }).rejects.toThrow(HttpException);
+    });
+
+    it("should send HTTP 500 if unable to find user", async () => {
+      const dto = {
+        email: "false",
+        password: "true",
+        recaptchaToken: "true"
+      };
+
+      const result = await authController.login(dto, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(result).toEqual({
+        status: "error",
+        message: "Error finding user"
+      });
+    });
+
+    it("should set cookies if logging in user", async () => {
+      const dto = {
+        email: "true",
+        password: "true",
+        recaptchaToken: "true"
+      };
+
+      await authController.login(dto, res);
+
+      expect(res.cookie).toHaveBeenNthCalledWith(1, "verified", "", {
+        httpOnly: false,
+        expires: expect.any(Date)
+      });
+      expect(res.cookie).toHaveBeenNthCalledWith(2, "refresh_token", {}, {
+        httpOnly: true,
+        expires: expect.any(Date)
+      });
+      expect(res.cookie).toHaveBeenNthCalledWith(3, "access_token", {}, {
+        httpOnly: true,
+        expires: expect.any(Date)
+      });
+      expect(res.cookie).toHaveBeenNthCalledWith(4, "authenticated", true, {
+        httpOnly: false,
+        expires: expect.any(Date)
+      });
+    });
+  });
+
+  describe("when POST /logout method is called", () => {
+    const res = {
+      status: jest.fn(),
+      cookie: jest.fn(),
+      redirect: jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any as Response;
+
+    it("should call the logout method", async () => {
+      const req = {} as any as Request;
+
+      await authController.logout(req, res);
+
+      expect(authService.logout).toHaveBeenCalled();
     });
   });
 });
