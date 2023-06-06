5b1e72e1d64213b28a174da60f8568ac9b0574dd
hwgilbert16 <hwgilbert16@gmail.com>
2023-06-05 21:20:05 -0400

    Fix head script implementation and fix homepage spinner margins

--- apps/front/src/app/head-scripts/head-scripts.component.ts
+++ apps/front/src/app/head-scripts/head-scripts.component.ts
@@ -10,9 +10,9 @@ export class HeadScriptsComponent implements OnInit {
   ngOnInit(): void {
     if (process.env["SCHOLARSOME_HEAD_SCRIPTS_BASE64"]) {
       const decoded = atob(process.env["SCHOLARSOME_HEAD_SCRIPTS_BASE64"]);
-      const element = new DOMParser().parseFromString(decoded, "text/html");
+      const fragment = document.createRange().createContextualFragment(decoded);
 
-      document.getElementsByTagName("head")[0].appendChild(element.firstChild?.firstChild?.firstChild as HTMLElement);
+      document.getElementsByTagName("head")[0].appendChild(fragment);
     }
   }
 }
