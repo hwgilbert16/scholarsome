e42ca38dedb9635d4c20eb3ccb772e839031ad8e
hwgilbert16 <hwgilbert16@gmail.com>
2024-01-04 20:49:42 -0500

    Add initial new landing page

--- apps/front/src/app/landing/landing.component.ts
+++ apps/front/src/app/landing/landing.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { ModalService } from "../shared/modal.service";
 import { CookieService } from "ngx-cookie";
 import { Router } from "@angular/router";
-import { DeviceDetectorService } from "ngx-device-detector";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { Location } from "@angular/common";
 import { Meta, Title } from "@angular/platform-browser";
+import { SharedService } from "../shared/shared.service";
 
 @Component({
   selector: "scholarsome-landing",
@@ -20,21 +21,20 @@ export class LandingComponent implements OnInit {
     private readonly cookieService: CookieService,
     private readonly router: Router,
     private readonly location: Location,
-    private readonly deviceService: DeviceDetectorService,
-    public readonly modalService: ModalService,
     private readonly titleService: Title,
-    private readonly metaService: Meta
+    private readonly metaService: Meta,
+    public readonly modalService: ModalService,
+    public readonly sharedService: SharedService
   ) {
     this.titleService.setTitle("Studying done the correct way — Scholarsome");
     this.metaService.addTag({ name: "description", content: "Scholarsome is the way studying was meant to be. No monthly fees or upsells to get between you and your study tools. Just flashcards." });
   }
 
-  isDesktop: boolean;
-  faGithub = faGithub;
+  protected stargazers: number;
+  protected readonly faGithub = faGithub;
+  protected readonly faUpRightFromSquare = faUpRightFromSquare;
 
   async ngOnInit(): Promise<void> {
-    this.isDesktop = this.deviceService.isDesktop();
-
     if (this.cookieService.get("authenticated") === "true") {
       this.location.go("homepage");
       await this.router.navigate(["homepage"]);
@@ -43,5 +43,7 @@ export class LandingComponent implements OnInit {
     if (this.cookieService.get("resetPasswordToken")) {
       this.modalService.modal.next("set-password-open");
     }
+
+    this.stargazers = await this.sharedService.getStargazers();
   }
 }
--- apps/front/src/app/shared/shared.service.ts
+++ apps/front/src/app/shared/shared.service.ts
@@ -8,17 +8,24 @@ import packageJson from "../../../../../package.json";
 })
 export class SharedService {
   constructor(private readonly http: HttpClient) {
-    this.res = lastValueFrom(this.http.get("https://api.github.com/repos/hwgilbert16/scholarsome/releases"));
+    this.releaseCheckRes = lastValueFrom(this.http.get("https://api.github.com/repos/hwgilbert16/scholarsome/releases"));
+    this.starsRes = lastValueFrom(this.http.get("https://api.github.com/repos/hwgilbert16/scholarsome"));
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private readonly res: Promise<any>;
+  private readonly releaseCheckRes: Promise<any>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private readonly starsRes: Promise<any>;
 
   async isUpdateAvailable(): Promise<boolean> {
-    return (await this.res)[0]["name"] !== "v" + packageJson.version;
+    return (await this.releaseCheckRes)[0]["name"] !== "v" + packageJson.version;
   }
 
   async getReleaseUrl(): Promise<string> {
-    return (await this.res)[0]["html_url"];
+    return (await this.releaseCheckRes)[0]["html_url"];
+  }
+
+  async getStargazers(): Promise<number> {
+    return (await this.starsRes)["stargazers_count"];
   }
 }
