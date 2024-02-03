8fb8f152cc197a86758dbfc879af91a7b98cff81
ArcSoftwareX <arc.software.x@gmail.com>
2024-02-03 22:35:44 +0300

    (api) chore: finish the StorageService

--- apps/api/src/app/providers/storage/storage.service.ts
+++ apps/api/src/app/providers/storage/storage.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { StorageProvider } from './interfaces/storage-provider.interface';
+import { ConfigService } from '@nestjs/config';
+import { StorageConfig } from './storage.config';
+import { StorageType } from './interfaces/storage-type.enum';
+import { S3StorageProvider } from './provider/s3.storage';
+import { LocalStorageProvider } from './provider/local.storage';
 
 @Injectable()
 export class StorageService {
@@ -8,5 +13,25 @@ export class StorageService {
   /**
    * @ignore
    */
-  constructor() {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly storageConfig: StorageConfig
+  ) {
+    const storageOptions = storageConfig.createStorageOptions();
+
+    this.storageProvider =
+      storageOptions.storageType === StorageType.S3
+        ? new S3StorageProvider(configService)
+        : new LocalStorageProvider(configService);
+  }
+
+  /**
+   *
+   * Get the StorageProvider instance.
+   *
+   * @returns StorageProvider instance.
+   */
+  public getInstance(): StorageProvider {
+    return this.storageProvider;
+  }
 }
