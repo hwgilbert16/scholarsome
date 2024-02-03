d4b7e249582e0cd33fa85683f7f16094c0849816
ArcSoftwareX <arc.software.x@gmail.com>
2024-02-03 22:40:10 +0300

    (api) feat: add StorageModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,105 +2,107 @@ import {
   MiddlewareConsumer,
   Module,
   NestModule,
-  RequestMethod
-} from "@nestjs/common";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
-import { AuthModule } from "./auth/auth.module";
-import { DatabaseModule } from "./providers/database/database.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { SetsModule } from "./sets/sets.module";
-import { MailModule } from "./providers/mail/mail.module";
-import { HttpsRedirectMiddleware } from "./providers/https-redirect.middleware";
-import { CardsModule } from "./cards/cards.module";
-import { UsersModule } from "./users/users.module";
-import { RedisModule } from "@liaoliaots/nestjs-redis";
-import { JwtModule } from "@nestjs/jwt";
-import { MediaModule } from "./media/media.module";
-import { TokenRefreshMiddleware } from "./providers/token-refresh.middleware";
-import { ConvertingModule } from "./converting/converting.module";
+  RequestMethod,
+} from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './providers/database/database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SetsModule } from './sets/sets.module';
+import { MailModule } from './providers/mail/mail.module';
+import { HttpsRedirectMiddleware } from './providers/https-redirect.middleware';
+import { CardsModule } from './cards/cards.module';
+import { UsersModule } from './users/users.module';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { JwtModule } from '@nestjs/jwt';
+import { MediaModule } from './media/media.module';
+import { TokenRefreshMiddleware } from './providers/token-refresh.middleware';
+import { ConvertingModule } from './converting/converting.module';
+import { StorageModule } from './providers/storage/storage.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "front"),
+      rootPath: join(__dirname, '..', 'front'),
       serveStaticOptions: {
         cacheControl: true,
-        maxAge: 31536000
+        maxAge: 31536000,
       },
-      exclude: ["/api/(.*)", "/handbook/(.*)"]
+      exclude: ['/api/(.*)', '/handbook/(.*)'],
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "docs"),
-      serveRoot: "/handbook",
+      rootPath: join(__dirname, '..', 'docs'),
+      serveRoot: '/handbook',
       serveStaticOptions: {
         cacheControl: true,
-        maxAge: 31536000
-      }
+        maxAge: 31536000,
+      },
     }),
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
     }),
     RedisModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         defaultOptions: {
-          host: configService.get<string>("REDIS_HOST"),
-          port: configService.get<number>("REDIS_PORT"),
-          username: configService.get<string>("REDIS_USERNAME"),
-          password: configService.get<string>("REDIS_PASSWORD")
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+          username: configService.get<string>('REDIS_USERNAME'),
+          password: configService.get<string>('REDIS_PASSWORD'),
         },
         config: [
           {
-            namespace: "apiToken",
-            keyPrefix: "apiToken&",
-            name: "apiToken"
+            namespace: 'apiToken',
+            keyPrefix: 'apiToken&',
+            name: 'apiToken',
           },
           {
-            namespace: "default"
-          }
-        ]
-      })
+            namespace: 'default',
+          },
+        ],
+      }),
     }),
     AuthModule,
     DatabaseModule,
     SetsModule,
     MailModule,
+    StorageModule,
     CardsModule,
     UsersModule,
     MediaModule,
     {
       ...JwtModule.registerAsync({
         useFactory: (configService: ConfigService) => ({
-          secret: configService.get("JWT_SECRET"),
-          signOptions: { expiresIn: "14d" }
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '14d' },
         }),
-        inject: [ConfigService]
+        inject: [ConfigService],
       }),
-      global: true
+      global: true,
     },
-    ConvertingModule
+    ConvertingModule,
   ],
   controllers: [],
   providers: [],
-  exports: [JwtModule]
+  exports: [JwtModule],
 })
 export class AppModule implements NestModule {
   constructor(private configService: ConfigService) {}
 
   configure(consumer: MiddlewareConsumer) {
     if (
-      this.configService.get<string>("NODE_ENV") === "production" &&
-      this.configService.get<string>("SSL_KEY_PATH") &&
-      this.configService.get<string>("SSL_KEY_PATH").length > 0
+      this.configService.get<string>('NODE_ENV') === 'production' &&
+      this.configService.get<string>('SSL_KEY_PATH') &&
+      this.configService.get<string>('SSL_KEY_PATH').length > 0
     ) {
       consumer
-          .apply(HttpsRedirectMiddleware)
-          .forRoutes({ path: "*", method: RequestMethod.ALL });
+        .apply(HttpsRedirectMiddleware)
+        .forRoutes({ path: '*', method: RequestMethod.ALL });
     }
 
     consumer
-        .apply(TokenRefreshMiddleware)
-        .forRoutes({ path: "*", method: RequestMethod.ALL });
+      .apply(TokenRefreshMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }
---
+++ apps/api/src/app/providers/storage/storage.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { StorageConfig } from './storage.config';
+import { StorageService } from './storage.service';
+
+@Module({
+  providers: [StorageConfig, StorageService],
+  exports: [StorageService],
+})
+export class StorageModule {}
