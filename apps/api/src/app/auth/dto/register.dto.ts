5cd6b8efd278136db10ad936b73ccbed7b3788dd
WhyNeet <arc.software.x@gmail.com>
2024-02-23 17:18:18 +0300

    (api) feat: add ValidationError and change error messages on RegisterDto

--- apps/api/src/app/auth/dto/register.dto.ts
+++ apps/api/src/app/auth/dto/register.dto.ts
@@ -1,19 +1,21 @@
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { ValidationError } from "@api/shared/exception/annotations/validationError.decorator";
 
+@ValidationError("Invalid registration credentials.")
 export class RegisterDto {
   @IsString()
-  @IsNotEmpty()
-    username: string;
+  @IsNotEmpty({ message: "username should not be empty" })
+  username: string;
 
-  @IsEmail()
-  @IsNotEmpty()
-    email: string;
+  @IsEmail(null, { message: "email must be valid" })
+  @IsNotEmpty({ message: "email should not be empty" })
+  email: string;
 
   @IsString()
-  @IsNotEmpty()
-    password: string;
+  @IsNotEmpty({ message: "password should not be empty" })
+  password: string;
 
   @IsString()
   @IsOptional()
-    recaptchaToken: string;
+  recaptchaToken: string;
 }
